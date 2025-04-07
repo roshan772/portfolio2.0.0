ee9155823f83ef76d078fadf1fdaeb7f56a17239
roshan772 <krishroishanroshan1221@gmail.com>
2025-04-07 10:16:18 +0530

    footer CSS & scroll up Button Js Updated

--- main.js
+++ main.js
@@ -60,8 +60,34 @@ let swipeTestimonial = new Swiper(".testimonial__container", {
 /*=============== EMAIL JS ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-
+const sections = document.querySelectorAll("section[id]");
+const scrollActive = () => {
+  const scrollY = window.pageYOffset;
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 58;
+    const sectionId = current.getAttribute("id");
+    const sectionsClass = document.querySelector(
+      `.nav__menu a[href*=${sectionId}]`
+    );
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
 /*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  if (this.scrollY >= 350) {
+    scrollUp.classList.add("show-scroll");
+  } else {
+    scrollUp.classList.remove("show-scroll");
+  }
+};
+window.addEventListener("scroll", scrollUp);
 
 /*=============== DARK LIGHT THEME ===============*/
 
