7cd62ab8a3c57ec228baebb1ff1d4135cec53ff3
roshan772 <krishroishanroshan1221@gmail.com>
2025-04-08 07:46:18 +0530

    Some Project Structure Changed

--- main.js
+++ main.js
@@ -132,3 +132,22 @@ const scrollHeader = () => {
 window.addEventListener("scroll", scrollHeader);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  reset: true, // set to true if you want animation every time you scroll back
+});
+// Example reveals
+sr.reveal(
+  `.home__data,.projects__container,.testimonial__container,.footer__container`
+);
+sr.reveal(`.home__info div`, { delay: 600, origin: "bottom", interval: 100 });
+sr.reveal(`.skills__content:nth-child(1),.contact__content:nth-child(1)`, {
+  origin: "left",
+});
+sr.reveal(`.skills__content:nth-child(2),.contact__content:nth-child(2)`, {
+  origin: "right",
+});
+sr.reveal(`.qualification__content,.services__card`, { interval: 100 });
